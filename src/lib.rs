7ed403e0dfae0b3dda855eed3917ff4bfcd27dec
Arthur Zopellaro <github@zope.me>
2024-07-08 22:53:25 -0300

    wip: unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
 pub fn foo() {
-    println!("hi");
+    let string: &str = "@article{1,\n\
+        author = {One, No},\n\
+        title = {Validating}\n\
+        }";
+    println!("{string}");
+}
+
+pub fn next_entry() -> u8 {
+    13
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::next_entry;
+    #[test]
+    fn it_works() {
+        let result = next_entry();
+        assert_eq!(result, 13);
+    }
 }
