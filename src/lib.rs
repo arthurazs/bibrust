afdc7b9b79e7b2a03c41c729ebe585e59b0c99d9
Arthur Zopellaro <github@zope.me>
2024-07-10 18:07:05 -0300

    wip: add logger, add next_entry

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,94 @@
-use std::io::Read;
-use std::fs::{read_dir, DirEntry, File};
+use log;
+// use std::fs::{read_dir, DirEntry, File, ReadDir};
+use std::fs::{read_dir, ReadDir};
+// use std::io::{Cursor, Read, Seek, SeekFrom, Write};
 
-fn read_file(entry: DirEntry) {
-    let mut bib = File::open(entry.path()).unwrap();
-    let mut buffer: [u8; 1] = [0; 1];
-    bib.read(&mut buffer).unwrap();
-    println!("{:?}", buffer);
-    bib.read(&mut buffer).unwrap();
-    println!("{:?}", buffer);
-}
+// fn next_entry(mut bib: &File) -> Cursor<Vec<u8>> {
+//     let mut buffer: [u8; 1] = [0; 1];
+//     let mut entry: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+//     let mut found: bool = false;
+//     let mut counter: usize = 0;
+//
+//     while bib.read(&mut buffer).unwrap() != 0 {
+//         entry.write(&buffer).unwrap();
+//
+//         let open: bool = buffer[0] == 0x7b;
+//         let close: bool = buffer[0] == 0x7d;
+//         if !found {
+//             found = open;
+//         }
+//
+//         counter += open as usize;
+//         counter -= close as usize;
+//
+//         if found && counter == 0 {
+//             break;
+//         }
+//     }
+//     entry.seek(SeekFrom::Start(0)).unwrap(); // rewind to the start of the stream
+//     return entry;
+// }
 
-pub fn foo() {
-    let path: &str = "data/input";
-    for folder in read_dir(path).unwrap().flatten() { // unwrap() == expect(msg)
-        for file in read_dir(folder.path()).unwrap().flatten() {
-            read_file(file);
-            break;
-        }
+// fn read_file(dir_entry: DirEntry) {
+//     let bib: File = File::open(dir_entry.path()).unwrap();
+//     let mut entry: Cursor<Vec<u8>> = next_entry(&bib);
+//     let mut buffer: String = String::new();
+//     entry.read_to_string(&mut buffer).unwrap();
+//     println!("<{}>", buffer);
+// }
+
+// fn iterate_base(mut read_dir: DirEntry) {
+//     while let Some(file) = read_dir.next() {
+//         log::info!("{file:?}");
+//         break;
+//     }
+// }
+
+fn iterate_root(mut read_dir: ReadDir) {
+    while let Some(folder) = read_dir.next() {
+        log::info!("{folder:?}");
         break;
     }
 }
 
-pub fn next_entry() -> u8 {
+pub fn foo() {
+    let path: &str = "data/input";
+    // unwrap() == expect(msg)
+    match read_dir(path) {
+        Ok(correct_path) => {
+            log::info!("{path}");
+            iterate_root(correct_path);
+        }
+        Err(e) => println!("\"{path}\": {e}"),
+    }
+    // match read_dir(path) {
+    //     Ok(mut folders) => {
+    //         while let Some(folder) = folders.next() {
+    //             println!("{:?}", folder);
+    //         }
+    //     }
+    //     Err(e) => println!("\"{path}\": {e}"),
+    // }
+    // if let Ok(folder) = read_dir(path) {
+    // for folder in read_dir(path).unwrap().flatten() {
+    //     for file in read_dir(folder.path()).unwrap().flatten() {
+    //         read_file(file);
+    //         break;
+    //     }
+    //     break;
+    // }
+}
+
+pub fn next_entrys() -> u8 {
     13
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::next_entry;
+    use crate::next_entrys;
     #[test]
     fn it_works() {
-        let result = next_entry();
+        let result = next_entrys();
         assert_eq!(result, 13);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use bibrust::foo;
 
 fn main() {
+    env_logger::Builder::new()
+        .filter(None, log::LevelFilter::Info)
+        .write_style(env_logger::WriteStyle::Always)
+        .init();
     foo();
 }
