b82c543fd2c7635ed826f265cd7950e77d1efaed
Arthur Zopellaro <github@zope.me>
2024-07-09 21:15:23 -0300

    wip: opening file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
+use std::io::Read;
+use std::fs::{read_dir, DirEntry, File};
+
+fn read_file(entry: DirEntry) {
+    let mut bib = File::open(entry.path()).unwrap();
+    let mut buffer: [u8; 1] = [0; 1];
+    bib.read(&mut buffer).unwrap();
+    println!("{:?}", buffer);
+    bib.read(&mut buffer).unwrap();
+    println!("{:?}", buffer);
+}
+
 pub fn foo() {
-    let string: &str = "@article{1,\n\
-        author = {One, No},\n\
-        title = {Validating}\n\
-        }";
-    println!("{string}");
+    let path: &str = "data/input";
+    for folder in read_dir(path).unwrap().flatten() { // unwrap() == expect(msg)
+        for file in read_dir(folder.path()).unwrap().flatten() {
+            read_file(file);
+            break;
+        }
+        break;
+    }
 }
 
 pub fn next_entry() -> u8 {
